879de203f5a4da6b422af1593946ce55d85490f7
root <root@localhost>
2021-09-29 04:54:59 +0000

    restructured and added DB attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root 'main#home'
+  resources :books
+  root 'books#index'
 
   get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
